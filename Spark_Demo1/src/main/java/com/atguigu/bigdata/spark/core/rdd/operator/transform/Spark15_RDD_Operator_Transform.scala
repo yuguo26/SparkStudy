a5154515113804f35abbe9c51f80e97243e53909
pengjiabin <pengjiabin@chinagoods.com>
2024-02-22 18:16:53 +0800

    #Update# 下班更新！

--- Spark_Demo1/src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark15_RDD_Operator_Transform.scala
+++ Spark_Demo1/src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark15_RDD_Operator_Transform.scala
@@ -13,7 +13,7 @@ object Spark15_RDD_Operator_Transform {
     // TODO 算子 - Key-Value类型 ReduceByKey
     val rdd = sc.makeRDD(List(("a", 1), ("a", 2), ("a", 3), ("b", 4)))
 
-    // reduceByKey: 想通的key的数据进行value数据的聚合操作
+    // reduceByKey: 相同的key的数据进行value数据的聚合操作
     // scala语言中一般的聚合操作都是两两聚合, spark是基于scala开发的, 所以spark也是两两聚合
     // reduceByKey 中，如果key的值只有一个那么这个值是不会参与计算的
     val reduceByKeyRDD: RDD[(String, Int)] = rdd.reduceByKey((x: Int, y: Int) => {
--- Spark_Demo1/src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark24_RDD_Req.scala
+++ Spark_Demo1/src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark24_RDD_Req.scala
@@ -12,9 +12,49 @@ object Spark24_RDD_Req {
 
     // TODO 案例实操
     // 1. 获取原始数据 时间戳, 省份, 城市, 用户, 广告
-    val sourceRDD: RDD[String] = sc.textFile("E:\\School\\project\\SparkStudy\\datas\\agent.log")
+    // val sourceRDD: RDD[String] = sc.textFile("E:\\School\\project\\SparkStudy\\datas\\agent.log")
+    val sourceRDD: RDD[String] = sc.textFile("datas/agent.log")
 
-    // 2. 将原始数据进行结构转换
+    // 2. 将原始数据进行结构转换。 方便统计
+    //    时间戳, 省份, 城市, 用户, 广告
+    //    ((省份, 广告), 1)
+    val mapRDD = sourceRDD.map(
+      line => {
+        val datas = line.split(" ")
+        ((datas(1), datas(3)), 1)
+      }
+    )
+
+    // 3. 将转换结构后的数据, 进行分组聚合
+    // ((省份, 广告), 1) => ((省份, 广告), sum)
+    val reduceRDD: RDD[((String, String), Int)] = mapRDD.reduceByKey(_ + _)
+'='
+    // 4. 将聚合的结果进行结构的转换
+    // ((省份, 广告), sum) => (省份, (广告, sum))
+    val newMapRDD = reduceRDD.map {
+      case ((prv, ad), sum) => {
+        (prv, (ad, sum))
+      }
+    }
+
+    // 5. 将转换后的结构根据省份进行分组
+    val groupRDD: RDD[(String, Iterable[(String, Int)])] = newMapRDD.groupByKey()
+
+    groupRDD.collect().foreach(println)
+    println("-----------------------------------")
+
+    // 6. 将分组后的数据组内排序(降序), 取前3名
+    val resultRDD: RDD[(String, List[(String, Int)])] = groupRDD.mapValues(
+      iter => {
+        iter.toList.sortBy(_._2)(Ordering.Int.reverse).take(3)
+      }
+    )
+
+    // 7. 将采集数据打印在控制台上
+    resultRDD.collect().foreach(println)
+
+
+
 
 
   }
